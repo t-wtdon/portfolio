477487493b7dd664d8edc3b3af8ce00327d5bc3f
watanabe-taichi <>
2026-03-18 17:18:11 +0900

    metaデータを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,12 @@ import { MainContent } from "@/components/MainContent/MainContent";
 import styles from "./page.module.scss";
 import { MouseLight } from "@/components/MouseLight/MouseLight";
 
+export const metadata = {
+  title: "ダイナソくん | フロントエンドエンジニア",
+  description:
+    "シンプルでわかりやすいWebサイト制作を大切にしています。Next.js/Reactを中心に、見やすく使いやすいWebアプリケーションを開発しています。",
+};
+
 const Home = () => {
   return (
     <div className={styles.layout}>
